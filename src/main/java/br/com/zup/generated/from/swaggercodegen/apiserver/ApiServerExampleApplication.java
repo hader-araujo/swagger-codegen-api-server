cb1f9a037691c548ae027888d7745f881dfd1117
Hader <hader.araujo@gmail.com>
2020-05-25 13:53:26 -0300

    initial commit

---
+++ src/main/java/br/com/zup/generated/from/swaggercodegen/apiserver/ApiServerExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.generated.from.swaggercodegen.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/generated/from/swaggercodegen/apiserver/ApiServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.generated.from.swaggercodegen.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
